3d0fad2489db18985971e754173d17589af5e30e
Simon <42054370+coeus-lei@users.noreply.github.com>
2024-10-17 11:43:53 +0530

    Create test_wework.py

---
+++ test_utils/test_wework.py
@@ -0,0 +1,10 @@
+from wework import WeWork
+import unittest
+
+class TestWework(unittest.TestCase):
+    def test_post_list_id(self):
+        print(WeWork().test_post_list_id(""))
+
+    def test_post_getuserid(self):
+        print(WeWork().test_post_getuserid("13111111111"))
+
